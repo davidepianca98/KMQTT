eed6e5503785bd7a3f76ae740a1adbdbdb9c7c5d
Davide Pianca <davidepianca98@gmail.com>
2020-01-30 15:21:57 +0100

    Add TopicUtils tests

--- src/commonMain/kotlin/mqtt/TopicUtils.kt
+++ src/commonMain/kotlin/mqtt/TopicUtils.kt
@@ -12,12 +12,13 @@ fun String.isValidTopic(): Boolean {
         if (this.count { it.toString().contains("#") } > 1 || (this != "#" && !this.endsWith("/#")))
             return false
     }
-    if (this.contains("+")) { // Taken from Paho MQTT Java
+    if (this.contains("+")) {
         for (i in 0 until length) {
             if (this[i] == '+') {
-                val prev = if (i - 1 >= 0) this[i - 1] else null
-                val next = if (i + 1 < length) this[i + 1] else null
-                if (prev != '/' && prev != null || next != '/' && next != null)
+                val previousCharacter = this.getOrNull(i - 1)
+                val nextCharacter = this.getOrNull(i + 1)
+
+                if (!((previousCharacter == '/' || previousCharacter == null) && (nextCharacter == '/' || nextCharacter == null)))
                     return false
             }
         }
@@ -26,13 +27,7 @@ fun String.isValidTopic(): Boolean {
     return true
 }
 
-// Taken from Paho MQTT Java
 fun String.matchesWildcard(wildcardTopic: String): Boolean {
-    var curn = 0
-    var curf = 0
-    val curnEnd: Int = this.length
-    val curfEnd: Int = wildcardTopic.length
-
     if (this.containsWildcard())
         return false
     if (!this.isValidTopic() || !wildcardTopic.isValidTopic())
@@ -46,22 +41,30 @@ fun String.matchesWildcard(wildcardTopic: String): Boolean {
         return true
     }
 
-    while (curf < curfEnd && curn < curnEnd) {
-        if (this[curn] == '/' && wildcardTopic[curf] != '/')
+    var positionTopic = 0
+    var positionTopicFilter = 0
+    while (positionTopic < this.length && positionTopicFilter < wildcardTopic.length) {
+        if (this[positionTopic] == wildcardTopic[positionTopicFilter]) {
+            positionTopic++
+            positionTopicFilter++
+            continue
+        }
+        if (wildcardTopic[positionTopicFilter] == '#') {
             break
-        if (wildcardTopic[curf] != '+' && wildcardTopic[curf] != '#' && wildcardTopic[curf] != this[curn])
-            break
-        if (wildcardTopic[curf] == '+') { // skip until we meet the next separator, or end of string
-            var nextpos = curn + 1
-            while (nextpos < curnEnd && this[nextpos] != '/')
-                nextpos = ++curn + 1
-        } else if (wildcardTopic[curf] == '#')
-            curn = curnEnd - 1 // skip until end of string
-        curf++
-        curn++
+        } else if (wildcardTopic[positionTopicFilter] == '+') {
+            while (positionTopic < this.length && this[positionTopic] != '/') {
+                positionTopic++
+            }
+            if (positionTopic == this.length)
+                break
+            else if (wildcardTopic.getOrNull(positionTopicFilter + 1) == '/')
+                positionTopicFilter++
+            else
+                return false
+        }
     }
 
-    return curn == curnEnd && curf == curfEnd
+    return true
 }
 
 fun String.isSharedTopicFilter(): Boolean {
--- src/commonTest/kotlin/mqtt/TestTopicUtils.kt
+++ src/commonTest/kotlin/mqtt/TestTopicUtils.kt
@@ -1,13 +1,24 @@
 package mqtt
 
 import kotlin.test.Test
+import kotlin.test.assertEquals
 import kotlin.test.assertFalse
 import kotlin.test.assertTrue
 
 class TestTopicUtils {
 
     @Test
-    fun testTopicValidation() {
+    fun testContainsWildcard() {
+        assertTrue { "sport/#".containsWildcard() }
+        assertTrue { "#".containsWildcard() }
+        assertTrue { "+".containsWildcard() }
+        assertTrue { "+/tennis/#".containsWildcard() }
+        assertFalse { "sport/".containsWildcard() }
+        assertFalse { "/tennis/test".containsWildcard() }
+    }
+
+    @Test
+    fun testIsValidTopic() {
         assertTrue { "sport/#".isValidTopic() }
         assertTrue { "#".isValidTopic() }
         assertTrue { "sport/tennis/#".isValidTopic() }
@@ -21,7 +32,28 @@ class TestTopicUtils {
     }
 
     @Test
-    fun testSharedTopicFilter() {
+    fun testMatchesWildcard() {
+        assertTrue { "sport/test".matchesWildcard("sport/#") }
+        assertTrue { "sport/test/1/2".matchesWildcard("sport/#") }
+        assertTrue { "sport/test".matchesWildcard("sport/+") }
+        assertFalse { "sport/test/1/2".matchesWildcard("sport/+") }
+        assertTrue { "sport/test/1/2".matchesWildcard("sport/+/1/2") }
+        assertTrue { "sport".matchesWildcard("+") }
+        assertFalse { "sport/1".matchesWildcard("+") }
+    }
+
+    @Test
+    fun testIsSharedTopicFilter() {
         assertTrue { "\$share/consumer1/sport/tennis/+".isSharedTopicFilter() }
+    }
+
+    @Test
+    fun testGetSharedTopicFilter() {
+        assertEquals("\$share/consumer1/sport/tennis/+".getSharedTopicFilter(), "sport/tennis/+")
+    }
+
+    @Test
+    fun testGetSharedTopicShareName() {
+        assertEquals("\$share/consumer1/sport/tennis/+".getSharedTopicShareName(), "consumer1")
     }
 }
