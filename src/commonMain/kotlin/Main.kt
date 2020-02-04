b4d4769bee52aaa527f4abf6fecf89e250668807
Davide Pianca <davidepianca98@gmail.com>
2020-02-04 17:12:34 +0100

    Add the Maven Publish plugin

--- src/commonMain/kotlin/Main.kt
+++ src/commonMain/kotlin/Main.kt
@@ -1,7 +1,7 @@
 import mqtt.Broker
 import socket.tls.TLSSettings
 
-fun main() {
+fun main() { // TODO add command line arguments for standalone execution
     Broker(
         tlsSettings = TLSSettings(keyStoreFilePath = "keyStore.p12", keyStorePassword = "changeit"),
         port = 8883
