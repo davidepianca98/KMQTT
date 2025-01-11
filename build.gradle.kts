83aec9e900d879866a31ddef789eacc90b20a0fd
davidepianca98 <davidepianca98@gmail.com>
2025-01-11 14:16:41 +0100

    Update dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,7 +7,7 @@ allprojects {
 
 buildscript {
     dependencies {
-        classpath("org.jetbrains.kotlinx:atomicfu-gradle-plugin:0.26.1")
+        classpath("org.jetbrains.kotlinx:atomicfu-gradle-plugin:0.27.0")
     }
 }
 
