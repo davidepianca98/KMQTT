a80d8305024d8e4fab036f5e40352725aea09881
Steyn Geldenhuys <Phaestion@users.noreply.github.com>
2025-04-25 14:45:21 +0200

    Clean up

--- kmqtt-socket/src/jsMain/kotlin/io/github/davidepianca98/currentTimeMillis.kt
+++
@@ -1,5 +0,0 @@
-package io.github.davidepianca98
-
-import kotlin.js.Date
-
-public actual fun currentTimeMillis(): Long = Date.now().toLong()
--- kmqtt-socket/src/jvmMain/kotlin/io/github/davidepianca98/currentTimeMillis.kt
+++
@@ -1,5 +0,0 @@
-package io.github.davidepianca98
-
-public actual fun currentTimeMillis(): Long {
-    return System.currentTimeMillis()
-}
