a5d4fb587051d3177a14dd4e5b606d00e274903a
Steyn Geldenhuys <Phaestion@users.noreply.github.com>
2025-04-25 15:09:02 +0200

    Wasm cmp sample working with step in loop - proof of concept working!

--- kmqtt-cmp-sample/src/wasmJsMain/kotlin/main.kt
+++ kmqtt-cmp-sample/src/wasmJsMain/kotlin/main.kt
@@ -5,7 +5,7 @@ import androidx.compose.ui.window.CanvasBasedWindow
 import io.github.davidepianca98.MQTTClient
 import io.github.davidepianca98.mqtt.Subscription
 import io.github.davidepianca98.mqtt.packets.Qos
-import io.ktor.utils.io.core.toByteArray
+import io.ktor.utils.io.core.*
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
@@ -17,38 +17,36 @@ fun main() {
     CanvasBasedWindow(canvasElementId = "canvas") {
 
         LaunchedEffect(Unit) {
-            launch(Dispatchers.Unconfined) {
-                println("Hello Ktor Client")
-                val client = MQTTClient(
-                    address = "127.0.0.1",
-                    port = 1884,
-                    publishReceived = {
-                        println(
-                            it.payload?.toByteArray()?.let { bytes -> "Publish received ${bytes.decodeToString()}" })
-                    },
-                    onSubscribed = {
-                        println("Received subscription message for $it")
-                    }
+            delay(5000)
+
+            println("Hello Ktor Client")
+            val client = MQTTClient(
+                address = "127.0.0.1",
+                port = 1884,
+                debugLog = true,
+                publishReceived = {
+                    println(
+                        it.payload?.toByteArray()?.let { bytes -> "Publish received ${bytes.decodeToString()}" })
+                },
+                onSubscribed = {
+                    println("Received subscription message for $it")
+                }
+            )
+
+            val subscription = Subscription("test-incoming")
+            client.subscribe(listOf(subscription))
+
+            while (true) {
+                client.step()
+                delay(1000)
+
+                println("Publishing...")
+                client.publish(
+                    retain = false,
+                    qos = Qos.EXACTLY_ONCE,
+                    topic = "test",
+                    payload = "hello world ${Clock.System.now()}".toByteArray().toUByteArray()
                 )
-
-                val subscription = Subscription("test-incoming")
-                client.subscribe(listOf(subscription))
-
-                launch(Dispatchers.Default) {
-                    client.run()
-                }
-
-                while (true) {
-                    delay(1000)
-
-                    println("Publishing...")
-                    client.publish(
-                        retain = false,
-                        qos = Qos.EXACTLY_ONCE,
-                        topic = "test",
-                        payload = "hello world ${Clock.System.now()}".toByteArray().toUByteArray()
-                    )
-                }
             }
         }
 
